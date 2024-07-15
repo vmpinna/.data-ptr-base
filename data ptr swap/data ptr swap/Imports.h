b4d8865fde575bf097627c2465350102ec65db11
vmpinna <ri1eytaymam@gmail.com>
2024-07-15 02:11:25 -0400

    Add files via upload

---
+++ data ptr swap/data ptr swap/Imports.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <ntdef.h>
+#include <ntifs.h>
+#include <ntddk.h>
+#include <windef.h>
+#include <ntstrsafe.h>
+#include <ntimage.h>
+
+typedef struct _LDR_DATA_TABLE_ENTRY
+{
+	LIST_ENTRY InLoadOrderLinks;
+	LIST_ENTRY InMemoryOrderLinks;
+	LIST_ENTRY InInitializationOrderLinks;
+	PVOID DllBase;
+	PVOID EntryPoint;
+	ULONG SizeOfImage;
+	UNICODE_STRING FullDllName;
+	UNICODE_STRING BaseDllName;
+} LDR_DATA_TABLE_ENTRY, * PLDR_DATA_TABLE_ENTRY;
+
+typedef struct _RTL_PROCESS_MODULE_INFORMATION
+{
+	HANDLE Section;
+	PVOID MappedBase;
+	PVOID ImageBase;
+	ULONG ImageSize;
+	ULONG Flags;
+	USHORT LoadOrderIndex;
+	USHORT InitOrderIndex;
+	USHORT LoadCount;
+	USHORT OffsetToFileName;
+	UCHAR  FullPathName[256];
+} RTL_PROCESS_MODULE_INFORMATION, * PRTL_PROCESS_MODULE_INFORMATION;
+
+typedef struct _RTL_PROCESS_MODULES
+{
+	ULONG NumberOfModules;
+	RTL_PROCESS_MODULE_INFORMATION Modules[1];
+} RTL_PROCESS_MODULES, * PRTL_PROCESS_MODULES;
+
+typedef struct _WRITE_STRUCT
+{
+	int special;
+
+	bool read;
+	bool write;
+	bool request_base;
+	bool new_base;
+	int target_pid;
+	void* base_address;
+	void* address;
+	void* buffer;
+	void* output;
+	void* process_base;
+
+	size_t size;
+
+	const char* mod_name;
+}WRITE_STRUCT;
+
+typedef enum _SYSTEM_INFORMATION_CLASS
+{
+	SystemBasicInformation,
+	SystemProcessorInformation,
+	SystemPerformanceInformation,
+	SystemTimeOfDayInformation,
+	SystemPathInformation,
+	SystemProcessInformation,
+	SystemCallCountInformation,
+	SystemDeviceInformation,
+	SystemProcessorPerformanceInformation,
+	SystemFlagsInformation,
+	SystemCallTimeInformation,
+	SystemModuleInformation = 0x0B
+} SYSTEM_INFORMATION_CLASS,
+* PSYSTEM_INFORMATION_CLASS;
